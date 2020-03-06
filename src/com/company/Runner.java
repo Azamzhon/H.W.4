a0e400c3168120041f3a672b750be7ddcb6a2409
Ziko <Azamzhon>
2020-03-06 19:46:08 +0600

    Initial commit

---
+++ src/com/company/Runner.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Runner extends Thread {
+    private int num = 1;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public void run() {
+        if (getNum() - 1 == 5) {
+            System.out.println(getName() + " берет палочку");
+            System.out.println(" Runner 5 идет к финишу ");
+            System.out.println(getName() + " бежит к Runner 4");
+        } else if (getNum() == 1) {
+            System.out.println(getName() + " бежит к Runner " + getNum());
+            System.out.println(" Runner " + getNum() + " берет палочку");
+        } else {
+            System.out.println(getName() + " берет палочку");
+            System.out.println(getName() + " бежит к Runner " + getNum());
+        }
+        try {
+            sleep(5000);
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }
+    }
+}
+
